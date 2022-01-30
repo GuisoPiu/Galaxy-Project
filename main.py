d17fc817065725c545c882b9b7a1b18c570b6e5a
GuisoPiu <gruposiice.hr@gmail.com>
2022-01-30 13:33:38 -0600

    Setup complete

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import Widget
+from kivy.app import App
+from kivy.uix.widget import  Widget
+
+
+class MainWidget(Widget):
+    pass
+
+class GalaxyApp(App):
+    pass
+
+
+GalaxyApp().run()
+
+        
