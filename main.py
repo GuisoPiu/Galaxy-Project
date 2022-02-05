86d03f2241b33b571a1249767a865f2deace83ad
GuisoPiu <gruposiice.hr@gmail.com>
2022-02-05 12:14:02 -0600

    Horizontal perspective

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ class MainWidget(Widget):
     V_LINES_SPACING = .1  # percentage in screen width
     vertical_lines = []  # empty array
 
-    H_NB_LINES = 4
-    H_LINES_SPACING = .2  # percentage in screen height
+    H_NB_LINES = 10
+    H_LINES_SPACING = .1  # percentage in screen height
     horizontal_lines = []
 
     def __init__(self, **kwargs):
@@ -92,15 +92,17 @@ class MainWidget(Widget):
         return int(x), int(y)
 
     def transform_perspective(self, x, y):
-        tr_y = y * self.perspective_point_y / self.height
-        if tr_y > self.perspective_point_y:
-            tr_y = self.perspective_point_y
+        lin_y = y * self.perspective_point_y / self.height
+        if lin_y > self.perspective_point_y:
+            lin_y = self.perspective_point_y
 
         diff_x = x - self.perspective_point_x
-        diff_y = self.perspective_point_y - tr_y
-        proportion_y = diff_y / self.perspective_point_y  # 1 when diff_y == self.height and 0 when diff_y == perspective_point_y
+        diff_y = self.perspective_point_y - lin_y
+        factor_y = diff_y / self.perspective_point_y  # 1 when diff_y == self.height and 0 when diff_y == perspective_point_y
+        factor_y = pow(factor_y, 2)
 
-        tr_x = self.perspective_point_x + diff_x * proportion_y
+        tr_x = self.perspective_point_x + diff_x * factor_y
+        tr_y = self.perspective_point_y - factor_y * self.perspective_point_y
 
         return int(tr_x), int(tr_y)
 
